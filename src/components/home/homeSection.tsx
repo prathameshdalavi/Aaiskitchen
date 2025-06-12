5256d8208990ced06b195690edd862412f5c6de3
Prathamesh Dalavi <prathameshdalavi04@gmail.com>
2025-06-12 12:35:44 +0530

    updated

--- src/components/home/homeSection.tsx
+++ src/components/home/homeSection.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import ts from 'typescript';
+
 
 export const HomeSection = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
